4711e324bad8c68fec3a52e5d934d146c413d55a
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-02-05 11:13:32 +0600

    feat: add cpus info

--- scripts/os/cpus.js
+++ scripts/os/cpus.js
@@ -1,3 +1,6 @@
-import { availableParallelism } from 'os';
+import os from 'os';
 
-export const cpus = () => console.log(availableParallelism());
+export const cpus = () => {
+  console.log(os.availableParallelism());
+  os.cpus().forEach((cpu) => console.log(`model: ${cpu.model.trim()}, clock rate: ${cpu.speed / 1000}GHz`));
+};

ec7dac674f38602097d70e450babe469fac48853
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-10-11 20:33:58 +0500

    fix: update change dir script

--- scripts/nwd/cd.js
+++ scripts/nwd/cd.js
@@ -1,9 +1,14 @@
-import { chdir } from 'process';
 import path from 'path';
+import { homedir } from 'os';
+import { chdir } from 'process';
 
 export const cd = async (pathToDirectory) => {
+  const newPath = path.resolve(pathToDirectory);
+
   try {
-    chdir(path.normalize(pathToDirectory));
+    if (newPath >= homedir()) {
+      chdir(newPath);
+    }
   } catch (err) {
     console.error('Operation failed');
   }

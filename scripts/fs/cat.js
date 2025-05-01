c727682a4204fdf392fc4d17d315d6b55261cb6a
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-05-01 06:55:35 +0500

    fix: fix cat script

--- scripts/fs/cat.js
+++ scripts/fs/cat.js
@@ -1,11 +1,13 @@
 import { createReadStream } from 'fs';
-import { stdout } from 'process';
-import { pipeline } from 'node:stream/promises';
 import path from 'path';
 
 export const cat = async (pathToFile) => {
   try {
-    await pipeline(createReadStream(path.normalize(pathToFile)), stdout);
+    const readStream = createReadStream(path.normalize(pathToFile), { encoding: 'utf-8' });
+
+    readStream.on('data', (chunk) => {
+      console.log(chunk);
+    });
   } catch {
     console.error('Operation failed');
   }
